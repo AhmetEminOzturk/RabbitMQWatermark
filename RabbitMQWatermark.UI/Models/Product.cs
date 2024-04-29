f2ec6e8ce37eff25ce13daa9b6187788e256fec7
Ahmet Emin Öztürk <eminahmetozturk@gmail.com>
2024-04-29 13:48:39 +0300

    init

---
+++ RabbitMQWatermark.UI/Models/Product.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQWatermark.UI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+        [Range(1,100)]
+        public int Stock { get; set; }
+        [StringLength(100)]
+        public string PictureUrl { get; set; }
+    }
+}
